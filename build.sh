2bd144a3f14177197e335d5b86c1131545223be6
davekowalski777 <davkovsky@gmail.com>
2024-12-09 18:54:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
