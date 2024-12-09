feaed8a3f5aa5d183984ebe434b48ac29b42f6f7
davekowalski777 <davkovsky@gmail.com>
2024-12-09 18:47:52 +0100

    Update wsgi.py

--- guitar_pedals/wsgi.py
+++ guitar_pedals/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guitar_pedals.settings_prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guitar_pedals.settings')
 
 application = get_wsgi_application()
-app = application  # Vercel needs the 'app' variable
