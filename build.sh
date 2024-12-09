f707f041a631133199a3a637ff17afaebee4d5f7
davekowalski777 <davkovsky@gmail.com>
2024-12-09 18:46:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "Installing dependencies..."
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Build completed!"
